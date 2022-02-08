06e00dc9b87d1a52dd17d8874587c000514126f1
AgustinEscudero <agustinescudero2613@gmail.com>
2022-02-08 09:26:03 +0100

    eliminar_productos
    elimina los productos y actualiza el carrito con el numero de productos restantes

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,31 @@ $(document).ready(function () {
     function carrito() {
         var cantidad = $(".producto").length;
         $("span").text(cantidad);
+        if(cantidad == 0){
+           $("#main").text("No hay productos en tu carrito");
+        }
     }
     carrito();
 
-   
+    $("#delete1").click(function(){
+        $("#prod1").remove();
+        carrito();
+      });
 
+    $("#delete2").click(function(){
+        $("#prod2").remove();
+        carrito();
+      });
 
+    $("#delete3").click(function(){
+        $("#prod3").remove();
+        carrito();
+      });
+
+      var suma = 0;
+    $('.precio').each(function(){
+        suma += parseFloat($(this).val());
+        $("#precio").text(suma);
+      });
+      
 });
